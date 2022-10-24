c6d6c45d646cf72dfc04cbb7c17c84a45edf6f5b
Tech-sis <abilawonadeola@gmail.com>
2022-10-24 18:28:36 -0400

    Fix styling issues

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,3 @@
-from api.v1.views.places import *
-from api.v1.views.users import *
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
-from api.v1.views.states import *
 from api.v1.views.index import *
 from models.review import Review
 from models.place import Place
@@ -12,12 +7,4 @@ from models.city import City
 from models.state import State
 from models import storage
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-"""import storage engine and classes"""
-
-"""import flask views"""
-from api.v1.views.places_reviews import *
-
-""""import storage engine and classes"""
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
