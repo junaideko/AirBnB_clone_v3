b0537cc8dd174b7b6089205d37bc8ec99b4da5a4
Tech-sis <abilawonadeola@gmail.com>
2022-10-24 18:08:01 -0400

    create api folder

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,21 @@
+from api.v1.views.places import *
+from api.v1.views.users import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.states import *
+from api.v1.views.index import *
+from models.review import Review
+from models.place import Place
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models import storage
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+"""import storage engine and classes"""
+
+"""import flask views"""
+from api.v1.views.places_reviews import *
